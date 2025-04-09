18128e538342ce089086d8e6f292bd144fb181a6
extraBottle <128778356+extraBottle@users.noreply.github.com>
2025-04-09 23:51:27 +0900

    Change port

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  SERVER_PORT: process.env.SERVER_PORT || 8080,
+  SERVER_PORT: process.env.SERVER_PORT || 1999,
   DOMAIN: 'https://port-0-clova-extension-m70rcpyrd73d7d9c.sel4.cloudtype.app/'
 }
